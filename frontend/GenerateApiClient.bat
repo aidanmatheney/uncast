4722fc9d98ecb88fe2fe70293c8a21637d1cbaf0
aidanmatheney <aidanmatheney@gmail.com>
2020-03-05 21:24:51 -0600

    Make sure we have Java before running API client generator

--- frontend/GenerateApiClient.bat
+++ frontend/GenerateApiClient.bat
@@ -10,6 +10,14 @@ SET GeneratorFileName=openapi-generator-cli-%GeneratorVersion%.jar
 SET GeneratorFilePath=%GeneratorFileName%
 SET GeneratorDownloadUrl=https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/%GeneratorVersion%/%GeneratorFileName%
 
+REM Exit with an error if we don't have Java
+WHERE java >nul 2>nul
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO ERROR: Java not found in path. Java must be installed to run the API client generator. 1>&2
+    EXIT /B 1
+)
+
+REM Download OpenAPI Generator if we don't already have it
 IF NOT EXIST "%GeneratorFilePath%" (
     ECHO Downloading %GeneratorFileName%...
     powershell Invoke-WebRequest -OutFile "%GeneratorFilePath%" "%GeneratorDownloadUrl%"
