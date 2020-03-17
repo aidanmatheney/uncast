25504a926e9b731e2038959e7d74178083dd7497
parkerkcombs <parkerkcombs@gmail.com>
2020-03-17 13:34:06 -0500

    Changed Sign In, Register, and Sign Out buttons to use Ant.

--- frontend/src/features/authentication/AuthenticationMenu.tsx
+++ frontend/src/features/authentication/AuthenticationMenu.tsx
@@ -3,6 +3,8 @@ import { useSelector, useDispatch } from 'react-redux';
 import { User } from 'oidc-client';
 import { RootState } from '../../app/createRootReducer';
 import { register, signIn, signOut } from './authenticationSlice';
+import { Button } from 'antd';
+import "antd/dist/antd.css";
 
 const Container: FunctionComponent = ({ children }) => {
   return (
@@ -15,8 +17,8 @@ const SignedOutView: FunctionComponent = () => {
 
   return (
     <Container>
-      <div><button type="button" onClick={() => dispatch(signIn())}>Sign In</button></div>
-      <div><button type="button" onClick={() => dispatch(register())}>Register</button></div>
+      <div><Button type="primary" onClick={() => dispatch(signIn())}>Sign In</Button></div>
+      <div><Button type="default" onClick={() => dispatch(register())}>Register</Button></div>
     </Container>
   );
 };
@@ -27,7 +29,7 @@ const SignedInView: FunctionComponent<{ user: User; }> = ({ user }) => {
   return (
     <Container>
       <div>Hello, {user.profile.name}</div>
-      <div><button type="button" onClick={() => dispatch(signOut())}>Sign Out</button></div>
+      <div><Button type="primary" onClick={() => dispatch(signOut())}>Sign Out</Button></div>
     </Container>
   );
 };
