7bf29bf694dc53229355ac7ca8a47467de4a647e
Parker Combs <41436545+parkerirl@users.noreply.github.com>
2020-05-09 17:39:24 -0500

    added theme documentation

--- frontend/src/features/theme/Theme.tsx
+++ frontend/src/features/theme/Theme.tsx
@@ -4,13 +4,13 @@ import 'react-h5-audio-player/lib/styles.css';
 
 //standard purple theme
 const ThemeStandard = { 
-    background: "#8c7ce2",
-    color: "#ffffff",
-    input: "#000000",
-    pageBackground: "#443063",
-    addBackground: "#b5ade0",
-    borderColor: "#ffffff",
-    pageBgColor: "#181818",
+    background: "#8c7ce2",    //"main color", like for buttons
+    color: "#ffffff",         //text color
+    input: "#000000",         //currently unused
+    pageBackground: "#443063",//background color of bottom container (auth menu, add stream menu, & navbar)
+    addBackground: "#b5ade0", //background color of file upload/rss input/youtube input in forms
+    borderColor: "#ffffff",   //border color for buttons
+    pageBgColor: "#181818",   //library/catalog/profile page background color for top container
     font: "Arial"
   };
 const ThemeStandardChildren = ({ children }: any) => (
@@ -26,7 +26,8 @@ const ThemeDark = {
   pageBackground: "#181818",
   addBackground: "#9c9c9c",
   borderColor: "#ffffff",
-  pageBgColor: "#000000"
+  pageBgColor: "#000000",
+  font: "Arial"
 };
 const ThemeDarkChildren = ({ children }: any) => (
 <ThemeProvider theme={ThemeDark}>{children}</ThemeProvider>
@@ -41,7 +42,8 @@ const ThemeLight = {
   pageBackground: "#e3e0d1",
   addBackground: "#d6d3bf",
   borderColor: "#181818",
-  pageBgColor: "#ffffff"
+  pageBgColor: "#ffffff",
+  font: "Arial"
 };
 const ThemeLightChildren = ({ children }: any) => (
 <ThemeProvider theme={ThemeLight}>{children}</ThemeProvider>
