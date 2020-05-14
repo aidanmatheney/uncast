e4864cda72bb592cd754de9036e6c3dc9958cc16
Parker Combs <41436545+parkerirl@users.noreply.github.com>
2020-05-14 01:23:49 -0500

    styled episode cards

--- frontend/src/features/podcast/EpisodeCard.tsx
+++ frontend/src/features/podcast/EpisodeCard.tsx
@@ -1,9 +1,67 @@
 import React, { FunctionComponent } from 'react';
 import styled from 'styled-components';
+import { FaPlus } from 'react-icons/fa';
+import { IconType } from 'react-icons/lib';
 
 import { Episode } from '../../common/entities';
 
-const Container = styled.div``;
+const Container = styled.div`
+  margin: 0.25rem;
+  color: ${props => props.theme.color};
+  font-size: 1em;
+  text-align: left;
+  border: 2px solid ${props => props.theme.borderColor};
+  border-radius: 3px;
+  background: ${props => props.theme.background};
+`;
+
+const EpisodeActivityContainer = styled.div`
+  margin: 0.25rem;
+  color: ${props => props.theme.color};
+  font-size: 1em;
+  text-align: right;
+  background: transparent;
+`;
+
+const EpisodeAddButton = styled.button`
+  margin: 0.25rem;
+  color: ${props => props.theme.color};
+  font-size: 1em;
+  text-align: center;
+  border-radius: 3px;
+  background: ${props => props.theme.pageBackground};
+`;
+
+const EpisodeTab: {
+  tab: string;
+  name: string;
+  Icon: IconType;
+}[] = [
+  {
+    tab: 'Play Episode',
+    name: 'Play Episode',
+    Icon: FaPlus
+  }
+];
+
+const IconContainer = styled.div``;
+const TextContainer = styled.div``;
+
+const EpisodeActivities: FunctionComponent = () => {
+  return (
+    <EpisodeActivityContainer>
+      {EpisodeTab.map(({ tab, name, Icon }) => (
+        <div>
+          <EpisodeAddButton key={tab} >
+            <IconContainer><Icon /></IconContainer>
+            <TextContainer>{name}</TextContainer>
+          </EpisodeAddButton>
+        </div>
+      ))}
+    </EpisodeActivityContainer>
+  )
+}
+
 
 const EpisodeCard: FunctionComponent<{
   episode: Episode;
@@ -13,7 +71,15 @@ const EpisodeCard: FunctionComponent<{
 
   return (
     <Container>
-      <pre>EpisodeCard: {JSON.stringify(episode)}</pre>
+      <pre>
+        {/*EpisodeCard: {JSON.stringify(episode)}*/}
+        {episode.name}
+        <br />
+        <i>{episode.durationS}</i>
+        <br />
+        Released: {episode.date}
+        <EpisodeActivities />
+      </pre>
     </Container>
   );
 };
